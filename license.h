f4bd65e081cad0688f816ad3a74f60c513db2a1c
tcpfailed <support@bulletfirewall.us>
2025-05-13 20:34:19 -0400

    Create license.h

---
+++ license.h
@@ -0,0 +1,59 @@
+/*
+ * TCP Watch - Network Monitoring and DDoS Protection Tool
+ * Version 1.0.1
+ * 
+ * Copyright (c) 2025 tcpfailed
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of TCP Watch nor the names of its contributors may be used
+ *    to endorse or promote products derived from this software without specific
+ *    prior written permission.
+ * 
+ * 4. Commercial use of this software requires explicit permission from the author.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * Contact Information:
+ * Author: tcpfailed
+ * Discord: tcprouting
+ * 
+ * This software includes the following features:
+ * - Real-time network traffic monitoring
+ * - DDoS attack detection and prevention
+ * - IP blacklisting
+ * - System resource monitoring
+ * - Web interface for monitoring
+ * 
+ * For the latest updates and documentation, visit:
+ * https://github.com/TCPTHEGOAT/tcp-watch
+ * 
+ * Last Updated: May 2025
+ */
+
+#ifndef TCP_WATCH_LICENSE_H
+#define TCP_WATCH_LICENSE_H
+
+#define TCP_WATCH_VERSION "1.0.1"
+#define TCP_WATCH_COPYRIGHT "Copyright (c) 2024 tcpfailed"
+#define TCP_WATCH_LICENSE "BSD 3-Clause License with Commercial Use Restriction"
+
+#endif /* TCP_WATCH_LICENSE_H */
