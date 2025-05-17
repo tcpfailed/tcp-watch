d1b3dd062b5dfe03566e5ba62465bf50695ada63
tcpfailed <support@bulletfirewall.us>
2025-05-16 21:24:16 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-wget "https://github.com/TCPTHEGOAT/tcp-watch/raw/main/tcpwatch.go" --no-check-certificate -O main
+wget "https://github.com/tcpfailed/tcp-watch/raw/main/tcpwatch.go" --no-check-certificate -O main
 echo "tcpwatch main file has been updated! | output: tcpwatch.go has been updated! |"
