90a463692ce5f0b5d9adc6f6fbbdc7a33ddd562a
tcpfailed <support@bulletfirewall.us>
2025-06-18 16:51:28 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 wget "https://github.com/tcpfailed/tcp-watch/raw/main/tcpwatch.go" --no-check-certificate -O tcpwatch.go
 echo "tcpwatch main file has been updated! | output: tcpwatch.go has been updated! |"
+wget "https://github.com/tcpfailed/tcp-watch/raw/main/server.js" --no-check-certificate -O server.js
+echo "tcpwatch web server has been updated! | output: server.js has been updated! |"
+wget "https://github.com/tcpfailed/tcp-watch/raw/main/abusedb.go" --no-check-certificate -O abusedb.go
+echo "tcpwatch abuse ip database report has been updated! | output: abusedb.go has been updated! |"
+wget "https://github.com/tcpfailed/tcp-watch/raw/main/bpfmaker.go" --no-check-certificate -O bpfmaker.go
+echo "tcpwatch bpf creation has been updated! | output: bpfmaker.go has been updated! |"
+
+
